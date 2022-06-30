a61ec4d4b5cbb79c74312214eefe364d5c7a766c
nik06hil <55582874+nik06hil@users.noreply.github.com>
2022-06-30 10:45:27 +0530

    first commit

---
+++ lib/question.dart
@@ -0,0 +1,14 @@
+class Question {
+  String questionText = '';
+  bool questionAnswer = false;
+
+  // constructor -> here we rewrite the constructor in place of
+  /*
+      Question(String questionText, bool questionAnswer){
+         this.questionText = questionText;
+         this.questionAnswer = questionAnswer;
+
+      }
+   */
+  Question(this.questionText, this.questionAnswer);
+}
